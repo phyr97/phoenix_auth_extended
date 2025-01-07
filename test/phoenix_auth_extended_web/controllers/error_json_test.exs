77836aa54b164b454279bae85715893d190ff0a6
Mats Klein <mats.klein@naymspace.de>
2025-01-07 21:49:13 +0100

    install phoenix project

---
+++ test/phoenix_auth_extended_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule PhoenixAuthExtendedWeb.ErrorJSONTest do
+  use PhoenixAuthExtendedWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert PhoenixAuthExtendedWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert PhoenixAuthExtendedWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
---
+++ test/phoenix_auth_extended_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixAuthExtendedWeb.PageControllerTest do
+  use PhoenixAuthExtendedWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
