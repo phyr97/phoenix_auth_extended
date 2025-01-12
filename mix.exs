72fcbc33856356bf89ea05d7f6e1596016a9b17a
Mats Klein <mats.klein@naymspace.de>
2025-01-12 23:31:28 +0100

    install webauthn_components

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule PhoenixAuthExtended.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:webauthn_components, path: "deps/webauthn_components"}
     ]
   end
 
