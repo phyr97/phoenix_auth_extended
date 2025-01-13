c0c23bcbfe8f25ff365ede2bcaa6bea04186ef45
Mats Klein <mats.klein@naymspace.de>
2025-01-13 20:46:07 +0100

    replace changesets with auth specific changesets

--- lib/phoenix_auth_extended/identity/user.ex
+++ lib/phoenix_auth_extended/identity/user.ex
@@ -1,6 +1,9 @@
 defmodule PhoenixAuthExtended.Identity.User do
   use Ecto.Schema
   import Ecto.Changeset
+  import PhoenixAuthExtended.Validation
+
+  alias PhoenixAuthExtended.Identity.UserToken
 
   schema "users" do
     field :email, :string
@@ -8,84 +11,20 @@ defmodule PhoenixAuthExtended.Identity.User do
     field :hashed_password, :string, redact: true
     field :current_password, :string, virtual: true, redact: true
     field :confirmed_at, :utc_datetime
+    has_many :tokens, UserToken, preload_order: [desc: :inserted_at]
 
     timestamps(type: :utc_datetime)
   end
 
-  @doc """
-  A user changeset for registration.
+  @doc false
+  def passkey_changeset(user, attrs) do
+    fields = __MODULE__.__schema__(:fields)
 
-  It is important to validate the length of both email and password.
-  Otherwise databases may truncate the email without warnings, which
-  could lead to unpredictable or insecure behaviour. Long passwords may
-  also be very expensive to hash for certain algorithms.
-
-  ## Options
-
-    * `:hash_password` - Hashes the password so it can be stored securely
-      in the database and ensures the password field is cleared to prevent
-      leaks in the logs. If password hashing is not needed and clearing the
-      password field is not desired (like when using this changeset for
-      validations on a LiveView form), this option can be set to `false`.
-      Defaults to `true`.
-
-    * `:validate_email` - Validates the uniqueness of the email, in case
-      you don't want to validate the uniqueness of the email (like when
-      using this changeset for validations on a LiveView form before
-      submitting the form), this option can be set to `false`.
-      Defaults to `true`.
-  """
-  def registration_changeset(user, attrs, opts \\ []) do
     user
-    |> cast(attrs, [:email, :password])
-    |> validate_email(opts)
-    |> validate_password(opts)
-  end
-
-  defp validate_email(changeset, opts) do
-    changeset
-    |> validate_required([:email])
-    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
-    |> validate_length(:email, max: 160)
-    |> maybe_validate_unique_email(opts)
-  end
-
-  defp validate_password(changeset, opts) do
-    changeset
-    |> validate_required([:password])
-    |> validate_length(:password, min: 12, max: 72)
-    # Examples of additional password validation:
-    # |> validate_format(:password, ~r/[a-z]/, message: "at least one lower case character")
-    # |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
-    # |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/, message: "at least one digit or punctuation character")
-    |> maybe_hash_password(opts)
-  end
-
-  defp maybe_hash_password(changeset, opts) do
-    hash_password? = Keyword.get(opts, :hash_password, true)
-    password = get_change(changeset, :password)
-
-    if hash_password? && password && changeset.valid? do
-      changeset
-      # If using Bcrypt, then further validate it is at most 72 bytes long
-      |> validate_length(:password, max: 72, count: :bytes)
-      # Hashing could be done with `Ecto.Changeset.prepare_changes/2`, but that
-      # would keep the database transaction open longer and hurt performance.
-      |> put_change(:hashed_password, Bcrypt.hash_pwd_salt(password))
-      |> delete_change(:password)
-    else
-      changeset
-    end
-  end
-
-  defp maybe_validate_unique_email(changeset, opts) do
-    if Keyword.get(opts, :validate_email, true) do
-      changeset
-      |> unsafe_validate_unique(:email, PhoenixAuthExtended.Repo)
-      |> unique_constraint(:email)
-    else
-      changeset
-    end
+    |> cast(attrs, fields)
+    |> validate_email(:email, unique: true)
+    # |> cast_assoc(:keys)
+    |> cast_assoc(:tokens)
   end
 
   @doc """
@@ -96,7 +35,7 @@ defmodule PhoenixAuthExtended.Identity.User do
   def email_changeset(user, attrs, opts \\ []) do
     user
     |> cast(attrs, [:email])
-    |> validate_email(opts)
+    |> validate_email(:email, opts)
     |> case do
       %{changes: %{email: _}} = changeset -> changeset
       %{} = changeset -> add_error(changeset, :email, "did not change")
@@ -119,7 +58,7 @@ defmodule PhoenixAuthExtended.Identity.User do
     user
     |> cast(attrs, [:password])
     |> validate_confirmation(:password, message: "does not match password")
-    |> validate_password(opts)
+    |> validate_password(:password, opts)
   end
 
   @doc """
@@ -136,7 +75,10 @@ defmodule PhoenixAuthExtended.Identity.User do
   If there is no user or the user doesn't have a password, we call
   `Bcrypt.no_user_verify/0` to avoid timing attacks.
   """
-  def valid_password?(%PhoenixAuthExtended.Identity.User{hashed_password: hashed_password}, password)
+  def valid_password?(
+        %PhoenixAuthExtended.Identity.User{hashed_password: hashed_password},
+        password
+      )
       when is_binary(hashed_password) and byte_size(password) > 0 do
     Bcrypt.verify_pass(password, hashed_password)
   end
